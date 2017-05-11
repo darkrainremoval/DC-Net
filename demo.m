4230c56a425fbb4ce82b1a37ef3d629ae48af7e8
darkrainremoval <dr.lazysheep@gmail.com>
2017-05-11 19:34:58 +0800

    Delete demo.m

--- demo.m
+++
@@ -1,9 +0,0 @@
-% =========================================================================
-% Test code for Going Clear from Misty Rain in Dark Channel Guided Network
-% =========================================================================
-
-clear; close all; clc;
-%% im_R is rainy image, im_DR is derained image
-im_R = imread('\im\lamp.jpg');
-[im_R,im_DR] = derain(im_R);
-figure,imshow([im_R,im_DR]);
